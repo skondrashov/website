5f28bc2c8aae14699618e1d21a9fc164804fb2b1
Timofey Kondrashov <tkondrashov@gmail.com>
2020-03-18 09:51:47 -0400

    add some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './components/App';
+import { render, fireEvent } from '@testing-library/react';
+
 import { MemoryRouter } from 'react-router-dom';
+import App from './components/App';
 
+test('App renders', () => {
+  const { container } = render(<MemoryRouter><App /></MemoryRouter>);
+  expect(container).toBeInTheDocument();
+});
 
-test('', () => {
-  const { getByText } = render(<MemoryRouter initialEntries={["/about"]}><App /></MemoryRouter>);
-  const linkElement = getByText(/I built this website with React/i);
-  expect(linkElement).toBeInTheDocument();
+test('App defaults to light mode', () => {
+  const { container } = render(<MemoryRouter><App /></MemoryRouter>);
+  const app = container.querySelector('.App');
+  expect(app).toHaveClass('light');
+});
+
+test('Darkmode button renders', () => {
+  const { container } = render(<MemoryRouter><App /></MemoryRouter>);
+  const darkmode = container.querySelector('#darkmode');
+  expect(darkmode).toBeInTheDocument();
+});
+
+test('Darkmode button toggles correctly', () => {
+  const { container } = render(<MemoryRouter><App /></MemoryRouter>);
+  const app = container.querySelector('.App');
+  const darkmode = container.querySelector('#darkmode');
+  expect(app).toHaveClass('light');
+  fireEvent.click(darkmode);
+  expect(app).toHaveClass('dark');
+  fireEvent.click(darkmode);
+  expect(app).toHaveClass('light');
+});
+
+import BlogPost from './components/BlogPost'
+
+test('BlogPost title renders', () => {
+  const data = {
+    timestamp: {seconds: 1000},
+    title: 'test title',
+    author: 'test author',
+    content: ['test content'],
+  };
+  const { getByText } = render(<BlogPost data={data}/>);
+  const title = getByText(/test title/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('BlogPost author renders', () => {
+  const data = {
+    timestamp: {seconds: 1000},
+    title: 'test title',
+    author: 'test author',
+    content: ['test content'],
+  };
+  const { getByText } = render(<BlogPost data={data}/>);
+  const author = getByText(/test author/i);
+  expect(author).toBeInTheDocument();
 });
